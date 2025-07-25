b3f7147e83e90297e64ee9740bdefd4ed6e7e4ac
SHYAMBHATANIYA45 <shyam1bhataniya@gmail.com>
2025-07-25 17:22:21 +0530

    Add files via upload

---
+++ JS/DOM/card.js
@@ -0,0 +1,5 @@
+function update(){
+  let input=document.getElementById("input")
+let result=document.getElementById("result")
+result.innerText=input.value
+}
---
+++ JS/DOM/vector.js
@@ -0,0 +1,11 @@
+//   let count = 0;
+  let re = document.getElementById("one");
+// function update(value){
+//   let re = document.getElementById("one");
+//    count+=1;
+//   re.innerText = count  ;
+// }
+
+function update(value){
+re.innerText =value
+}
